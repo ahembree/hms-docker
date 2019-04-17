c8f8afbedc902cff15de9cb39e3b3c167f6becb7
Aidan Hembree <8049680+Pr0meth3us@users.noreply.github.com>
2019-04-17 17:58:55 -0400

    handle nfs options, defaults if none

--- setup.sh
+++ setup.sh
@@ -155,12 +155,19 @@ config_network_share () {
   echo "${yellow}Your network share credentials are now stored in ${CREDENTIALFILE}${reset}"
   echo "${red}CAREFUL: THESE ARE STORED IN PLAINTEXT${reset}"
   echo "${yellow}Mounting network share..."
+
   if [[ $(grep -c "${NETWORKSHAREHOST}" "/etc/fstab") == 0 ]] ; then
       echo "# HMS-Docker Mount" | sudo tee -a /etc/fstab >/dev/null
       if [[ ${NETWORKSHAREDRIVER,,} == "cifs" ]] ; then
         echo "//${NETWORKSHAREHOST} ${DATAFOLDER} ${NETWORKSHAREDRIVER,,} vers=3.0,credentials=${CREDENTIALFILE},uid=$USER,gid=$USER 0 0" | sudo tee -a /etc/fstab >/dev/null
       elif [[ ${NETWORKSHAREDRIVER,,} == "nfs" ]] ; then
-        echo "${NETWORKSHAREHOST}:${NFSFOLDER} ${DATAFOLDER} ${NETWORKSHAREDRIVER} defaults 0 0" | sudo tee -a /etc/fstab >/dev/null
+        if [[ ${NFSOPTIONS} ]]; then
+          echo "${yellow}Creating with NFS options ${NFSOPTIONS}${reset}"
+          echo "${NETWORKSHAREHOST}:${NFSFOLDER} ${DATAFOLDER} ${NETWORKSHAREDRIVER} ${NFSOPTIONS} 0 0" | sudo tee -a /etc/fstab >/dev/null
+        elif [[ ! ${NFSOPTIONS} ]]; then
+          echo "${yellow}No NFS options specified, using default${reset}"
+          echo "${NETWORKSHAREHOST}:${NFSFOLDER} ${DATAFOLDER} ${NETWORKSHAREDRIVER} defaults 0 0" | sudo tee -a /etc/fstab >/dev/null
+        fi
       fi
       sudo mount -a
       echo "${green}fstab entry created, will mount ${yellow}${NETWORKSHAREHOST}${green} to ${yellow}${DATAFOLDER}${green} on boot.${reset}"
